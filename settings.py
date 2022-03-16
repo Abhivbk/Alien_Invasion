04524a2ce30624ee6a603517fad5b5c894abc902
Abhinav Kukarni <abhivbk13@gmail.com>
2022-03-16 12:18:06 +0530

    Initial Commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    #  """A class to store all settings for Alien Invasion."""
+    def __init__(self):
+        #  """Initialize the game's settings."""
+        # Screen settings
+        self.screen_height = 650
+        self.screen_width = 1000
+        self.bg_color = (44, 42, 74)
+
+        self.ship_speed_factor = 1.5
