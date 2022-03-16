82ea01638c1d9f2c8a43d0929ec25be9ad1584c4
Abhinav Kukarni <abhivbk13@gmail.com>
2022-03-16 13:07:34 +0530

    Added Support for WASD

--- game_functions.py
+++ game_functions.py
@@ -3,16 +3,16 @@ import pygame
 
 
 def check_keydown_events(event, ship):
-    if event.key == pygame.K_RIGHT:
+    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
         ship.moving_right = True
-    elif event.key == pygame.K_LEFT:
+    elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
         ship.moving_left = True
 
 
 def check_keyup_events(event, ship):
-    if event.key == pygame.K_RIGHT:
+    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
         ship.moving_right = False
-    elif event.key == pygame.K_LEFT:
+    elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
         ship.moving_left = False
 
 
